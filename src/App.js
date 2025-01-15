78911c9d38bf0cb9e6dd523d6f29ad98c86081f5
tpsdevlop <developers@thoughtprocess.co.in>
2025-01-15 13:21:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Demo from './Demo';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+       {/* <Routes>
+        <Route path="/" element={<Demo />} />
+      </Routes> */}
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Demo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function Demo() {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://surgebackend.azurewebsites.net//');
+        const data = response.data;
+
+        setCourses(data.Message);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  return (
+    <div>
+        {courses}
+    </div>
+  )
+}
+
+export default Demo
